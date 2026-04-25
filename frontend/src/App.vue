459602b8f10f6a0b75b3dbb637b463f3e956bff9
cliteratus07 <caesar_literatus@aai.com.ph>
2026-04-25 23:36:33 +0800

    v1

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <RouterView v-slot="{ Component }">
-    <Transition name="page" mode="out-in">
-      <component :is="Component" />
-    </Transition>
-  </RouterView>
+  <div>
+    <div style="position: fixed; top: 0; left: 0; background: red; color: white; z-index: 9999; padding: 5px;">DEBUG: APP MOUNTED</div>
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
+  </div>
 </template>
 
 <script setup>
